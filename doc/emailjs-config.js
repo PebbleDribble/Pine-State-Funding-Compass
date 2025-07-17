3da1077aea4495a71164da2f3d7be1182029865a
PebbleDribble <neuralball0108@gmail.com>
2025-07-17 08:38:24 +0530

    Add files via upload

---
+++ doc/emailjs-config.js
@@ -0,0 +1,4 @@
+// EmailJS configuration (do not expose secrets in public repos)
+const EMAILJS_PUBLIC_KEY = 'Wpm_zvsBjyNLXBvx7';
+const EMAILJS_SERVICE_ID = 'service_nfq40x7';
+const EMAILJS_TEMPLATE_ID = 'template_j9wax3l'; 
